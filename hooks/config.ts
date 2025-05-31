4d433d3dcdd7db0acf7fe2814c44405aab0df56b
XNeyMo <xneymodev@protonmail.com>
2025-05-31 17:55:52 -0500

    hotfix(hooks): api url in .env

--- hooks/config.ts
+++ hooks/config.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.API_URL || 'http://localhost:9200';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:9200';
 
 const API = axios.create({
 	baseURL: API_URL,
