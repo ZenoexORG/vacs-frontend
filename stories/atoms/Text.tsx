1547ef099233edbeee121b61a5a980c89392c86d
XNeyMo <xneymodev@protonmail.com>
2025-05-31 15:00:11 -0500

    hotfix(types): dashboard fixed

--- stories/atoms/Text.tsx
+++ stories/atoms/Text.tsx
@@ -4,12 +4,14 @@ export interface TextProps {
   isDark?: boolean;
   size?: 'small' | 'medium' | 'large' | 'smallest';
   children?: ReactNode;
+  className?: string;
 }
 
 export const Text = ({
   isDark = false,
   size = 'medium',
   children,
+  className = '',
 }: TextProps) => {
   const baseColor =
     isDark
@@ -25,7 +27,7 @@ export const Text = ({
 
   return (
     <div className="flex gap-2 items-center">
-      <p className={`duration-500 ease-in-out ${baseColor} ${sizes[size]}`}>
+      <p className={`duration-500 ease-in-out ${baseColor} ${sizes[size]} ${className}`}>
         {children}
       </p>
     </div>
